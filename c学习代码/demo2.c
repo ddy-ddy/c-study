b22090f47b285f375f37bf3f9bc60cdca5e33102
Yu Duan <1179730251@qq.com>
2022-04-09 22:56:23 +0800

    4.9 学习输入输出.

---
+++ c学习代码/demo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    printf("enter a value:");
+    c = getchar();
+    printf("\n you entered: ");
+    putchar(c);
+    printf("\n");
+    return 0;
+}
---
+++ c学习代码/demo3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    printf("enter a value:");
+    gets(str);
+    printf("\n you entered:");
+    puts(str);
+    return 0;
+}
